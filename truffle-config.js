859d02b14df03044787133e387f5def38163e907
Harsh Soni <algebra.soni@gmail.com>
2023-02-01 15:49:14 +0000

    matic network

--- truffle-config.js
+++ truffle-config.js
@@ -21,7 +21,17 @@ module.exports = {
       gas: 5000000,
       gasPrice: 25000000000,
       network_id: 3
-    }
+    },
+    matic: {
+      provider: () => new HDWalletProvider(process.env.SECRET_KEY, 
+      `https://rpc-mumbai.matic.today`),
+      network_id: 80001,
+      confirmations: 2,
+      timeoutBlocks: 200,
+      skipDryRun: true,
+      gas: 6000000,
+      gasPrice: 10000000000,
+    },
   },
   contracts_directory: './src/contracts/',
   contracts_build_directory: './src/abis/',
